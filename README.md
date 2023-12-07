17289a8fcca4c3afd34c2ea535087a367b4d2632
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-12-07 18:44:41 +0330

    feat: add sctipt and docs

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # easy-update
+
 Update your Debian based linux with ease.
+
+## Run
+
+Just copy and paste this line on your machine and hit enter:
+```bash
+curl
+```
+
+## Contribution
+
+Feel free to submit issues and PRs.
---
+++ easy_update.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+update() {
+    apt update
+    apt upgrade -y
+}
+
+check_kernel() {
+    current_kernel=$(uname -r)
+    latest_kernel=$(dpkg -l | awk '/linux-image-[0-9]/{print $2}' | sort -V | tail -n 1 | cut -d'-' -f3-)
+
+    if [ "$current_kernel" != "$latest_kernel" ]; then
+        echo "Kernel has been updated. Now, rebooting... 🔄"
+        reboot
+    else
+        echo "Kernel is up to date, so no need to reboot. 😀"
+    fi
+}
+
+main() {
+    update
+    if [ $? -eq 0 ]; then
+        check_kernel
+    else
+        echo "There was a problem while updating your package manager. 😢"
+}
+
+main
